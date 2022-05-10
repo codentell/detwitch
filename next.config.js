c03a33a3c27c6aff57a6c66bcd757e7020d53f8a
drewhoang <drewhoang@drewhoangs-MacBook-Pro.local>
2022-05-10 06:58:05 -0700

    add gitignore and env with moralis

---
+++ next.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    env: {
+        NEXT_APP_MORALIS_APPLICATION_ID: 'iuIhZ5l9ToH2eCQqRuJ8aShAiWP9j89YovDA6RiU',
+        NEXT_APP_MORALIS_SERVER_URL: 'https://x8uzjdl3hdri.usemoralis.com:2053/server'
+    },
+}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,15 @@
 import '../styles/global.css'
 import { MoralisProvider } from "react-moralis";
+
 // This default export is required in a new `pages/_app.js` file.
-export default function App({ Component, pageProps }) {
-  return  (<MoralisProvider appId={process.env.NEXT_APP_MORALIS_APPLICATION_ID} serverUrl={process.env.NEXT_APP_MORALIS_SERVER_URL}>
+
+const appID = process.env.NEXT_APP_MORALIS_APPLICATION_ID ;
+const serverUrl = process.env.NEXT_APP_MORALIS_SERVER_URL;
+
+
+
+export default function MyApp({ Component, pageProps }) {
+  return  (<MoralisProvider appId={appID} serverUrl={serverUrl}>
         <Component {...pageProps} />
   </MoralisProvider>);
 }
