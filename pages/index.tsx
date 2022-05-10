7f09ac0cca2fdd7f19ee9b5121ee81c6dea86089
drewhoang <drewhoang@drewhoangs-MacBook-Pro.local>
2022-05-10 12:02:04 -0700

    update live css for detwitch

--- pages/index.tsx
+++ pages/index.tsx
@@ -40,17 +40,26 @@ function Home() {
             <section style={{ display: "grid", gridTemplateColumns: "1fr 340px" }}>
                 <div>
                     <Player options={videoJsOptions} onReady={handlePlayerReady} />
-                    <section style={{display: "grid", gridTemplateColumns: "100px 1fr",  alignItems: "center",}}>
-                        <div style={{justifySelf: "center"}}>
-                            <Davatar
-                                size={70}
-                                //0x0e44BA4668CF6D33e05739906d0b66eb50F94A2D
-                                //0x0000000000000000000000000000000000000
-                                address='0x0e44BA4668CF6D33e05739906d0b66eb50F94A2D'
-                                //provider={provider} // optional
-                                //graphApiKey={apiKey} // optional
-                                generatedAvatarType='jazzicon' // optional, 'jazzicon' or 'blockies'
-                            />
+                    <section style={{ display: "grid", gridTemplateColumns: "100px 1fr", alignItems: "center", }}>
+                        <div style={{ justifySelf: "center" }}>
+                            <div style={{ background: "green", padding: "10px" }}>
+                                <div style={{ background: "orange", borderRadius: "100%", padding: "5px"}}>
+                                <Davatar
+                                    size={70}
+                                    //0x0e44BA4668CF6D33e05739906d0b66eb50F94A2D
+                                    //0x0000000000000000000000000000000000000
+                                    address='0x0e44BA4668CF6D33e05739906d0b66eb50F94A2D'
+                                    //provider={provider} // optional
+                                    //graphApiKey={apiKey} // optional
+                                    generatedAvatarType='jazzicon' // optional, 'jazzicon' or 'blockies'
+                                />
+                                  </div>
+                                <div className="live" style={{
+                                    textAlign: "center",
+                                    width: "50px",
+                                    margin: "0 auto", color: "white", fontWeight: "bold", background: "red", padding: "5px", borderRadius: "3px"
+                                }}>LIVE</div>
+                            </div>
                         </div>
                         <div>
                             <h1>Title</h1>
@@ -68,14 +77,14 @@ function Home() {
 
                         </div>
                         <section className="stream-chat-input" style={{ height: "130px", display: "grid", gridTemplateRows: "1fr 1fr" }}>
-                            <div style={{margin: "0px 10px"}}>
-                            <textarea style={{background: "#464649", width: "100%", borderRadius: "4px", border: "2px solid transparent"}} ></textarea>
+                            <div style={{ margin: "0px 10px" }}>
+                                <textarea style={{ background: "#464649", width: "100%", borderRadius: "4px", border: "2px solid transparent" }} ></textarea>
                             </div>
-                            <div style={{display: "grid", gridTemplateColumns: "2fr 70px", padding: "0px 10px 10px 10px"}}>
+                            <div style={{ display: "grid", gridTemplateColumns: "2fr 70px", padding: "0px 10px 10px 10px" }}>
                                 <div></div>
-                                <button style={{color: "white", fontWeight: "bold", background: "#9146FF", width: "70px", border: "2px solid #9146FF", borderRadius: "3px", height: "30px"}} >Chat</button>
+                                <button style={{ color: "white", fontWeight: "bold", background: "#9146FF", width: "70px", border: "2px solid #9146FF", borderRadius: "3px", height: "30px" }} >Chat</button>
                             </div>
-                          
+
                         </section>
 
                     </div>
