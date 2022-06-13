8891fcbf38bc6becda3e3f63798b40ef1fec9649
Victor Elias <victorgelias@gmail.com>
2022-06-13 18:45:43 -0300

    components: Run prettier over player

--- components/player.tsx
+++ components/player.tsx
@@ -1,53 +1,53 @@
-import React from "react";
-import videojs from "video.js";
-import "video.js/dist/video-js.css";
+import React from "react"
+import videojs from "video.js"
+import "video.js/dist/video-js.css"
 
 const Player = (props) => {
-  const videoRef = React.useRef(null);
-  const playerRef = React.useRef(null);
-  const { options, onReady } = props;
+  const videoRef = React.useRef(null)
+  const playerRef = React.useRef(null)
+  const { options, onReady } = props
 
   React.useEffect(() => {
     // make sure Video.js player is only initialized once
     if (!playerRef.current) {
-      const videoElement = videoRef.current;
-      if (!videoElement) return;
+      const videoElement = videoRef.current
+      if (!videoElement) return
 
       const player = (playerRef.current = videojs(videoElement, options, () => {
-        console.log("player is ready");
-        onReady && onReady(player);
-      }));
+        console.log("player is ready")
+        onReady && onReady(player)
+      }))
     } else {
       // you can update player here [update player through props]
       // const player = playerRef.current;
       // player.autoplay(options.autoplay);
       // player.src(options.sources);
     }
-  }, [options, videoRef]);
+  }, [options, videoRef])
 
   // Dispose the Video.js player when the functional component unmounts
   React.useEffect(() => {
-    const player = playerRef.current;
+    const player = playerRef.current
 
     return () => {
       if (player) {
-        player.dispose();
-        playerRef.current = null;
+        player.dispose()
+        playerRef.current = null
       }
-    };
-  }, [playerRef]);
+    }
+  }, [playerRef])
 
   return (
     <>
-        <div data-vjs-player>
-          <video
-            ref={videoRef}
-            className="video-js vjs-big-play-centered"
-            height="500px"
-          />
-        </div>
+      <div data-vjs-player>
+        <video
+          ref={videoRef}
+          className="video-js vjs-big-play-centered"
+          height="500px"
+        />
+      </div>
     </>
-  );
-};
+  )
+}
 
-export default Player;
+export default Player
