73bb7d84ee1ddffabb4dc48aa1f8553f18365eaa
Victor Elias <victorgelias@gmail.com>
2022-06-13 18:57:48 -0300

    config: Update config

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 module.exports = {
   env: {
+    // TODO: Remove these from the repository (also revoke the API key on the dashboard)
     LIVEPEER_API_KEY: "f46408f6-3cf4-4760-9051-fe6710225428",
-    LIVEPEER_PLAYBACK_URL:
-      "https://livepeercdn.com/hls/9ed3fzyh3aqaanxa/index.m3u8",
+    LIVEPEER_STREAM_ID: "9ed36315-72e0-4e12-8fcd-52ff154b671b",
     //NEXT_APP_MORALIS_APPLICATION_ID: 'jBIXs8qDgrH7qcIVOk9Q7zqRwQX9JhDbxD3RpZ5E',
     //NEXT_APP_MORALIS_SERVER_URL: 'https://oyfyipwo2rdn.usemoralis.com:2053/server'
   },
